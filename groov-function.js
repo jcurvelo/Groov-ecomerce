86ab2dd1d6701f474837e3bdd12cf7ee782bd886
Daniel <danielalexanderocanto@gmail.com>
2020-07-27 20:21:06 -0430

    push prueba

--- groov-function.js
+++ groov-function.js
@@ -45,12 +45,14 @@ Vue.component('item-box',{
     },
     template: `
     <div class="item-box mt-4">
-        <div class="item-image">
-            <img :src="product_img" :alt="product_img">
+        <div class="contenido">
+            <div class="item-image">
+                <img :src="product_img" :alt="product_img">
+            </div>
+            <div class="item-name">{{ product_name }}</div>
+            <div class="item-price">{{ product_price }}$</div>
+            <button v-on:click="item_id(product_id)">Agregar al carrito</button>
         </div>
-        <div class="item-name">{{ product_name }}</div>
-        <div class="item-price">{{ product_price }}$</div>
-        <button v-on:click="item_id(product_id)">Agregar al carrito</button>
     </div>    
     `
 })
